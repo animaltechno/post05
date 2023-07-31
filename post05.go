d3281c4ce569f4fb3846926200975cfbd08b230b
animaltechno <animaltechno@yahoo.co.jp>
2023-07-31 18:01:57 +0900

    exist関数

--- post05.go
+++ post05.go
@@ -9,3 +9,32 @@ func openConnection() (*sql.DB, error) {
 	}
 	return db, nil
 }
+
+// The function returns the User ID of the username
+// -1 if the user does not exist
+func exist(username string) int  {
+	username = strings.ToLower(username)
+
+	db, err := openConnection()
+	if err != nil {
+		fmt.Println(err)
+		return -1
+	}
+	defer db.Close()
+
+	userID := -1
+	statement := fmt.Sprintf(`SELECT "id" FROM "users" where username = '%s'`, username)
+	rows, err := db.Query(statement)
+
+	for rows.Next() {
+		var id int
+		err = rows.Scan(&id)
+		if err != nil {
+			fmt.Println(err)
+			return -1
+		}
+		userID = id
+	}
+	defer rows.Close()
+	return userID
+}
